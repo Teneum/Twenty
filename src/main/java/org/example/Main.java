c8ac7d8338d35e7d3e0bf42793f5ef721e8ab8ed
Teneum <2508.shashank@gmail.com>
2024-07-02 15:25:35 +0300

    Added Icon for App

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,8 +16,11 @@ public class Main extends JFrame{
     private void init(){
         setTitle("Twenty");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(new Dimension(800,600));
+        setSize(new Dimension(250,175));
+        setResizable(false);
         setLocationRelativeTo(null);
+        ImageIcon icon = new ImageIcon("src/main/resources/eye.png");
+        setIconImage(icon.getImage());
     }
 
     public static void main(String[] args){
